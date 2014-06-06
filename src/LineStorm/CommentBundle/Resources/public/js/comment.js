3408a7b901b507f7a77053789b2ecb890f454241
Andy Thorne <contrabandvr@gmail.com>
2014-06-06 16:11:05 +0000

    Updated scripts for requirejs

--- src/LineStorm/CommentBundle/Resources/public/js/comment.js
+++ src/LineStorm/CommentBundle/Resources/public/js/comment.js
@@ -1,152 +1,164 @@
-(function(){
-    $(document).ready(function(){
+define(['jquery', 'bootstrap', 'cms_api'], function($, bs, api){
 
-        var $commentBlock = $('.comment-block');
-        var $commentBlockNew = $commentBlock.find('.comment-block-new');
-        var $commentBlockComments = $commentBlock.find('.comment-block-comments');
+    // get the new comment form
+    var _load = function($commentBlock){
+            var
+                $commentBlockNew = $commentBlock.find('.comment-block-new'),
+                $commentBlockComments = $commentBlock.find('.comment-block-comments'),
 
-        // get the new comment form
-        var loadCommentForm = function(){
-            window.lineStorm.api.call($commentBlock.data('url-new'), {
-                dataType: 'json',
-                success: function(ob){
-                    if(ob.form){
-                        var $newForm = $(ob.form);
-                        $commentBlockNew.html($newForm);
+                _loadCommentForm = function(){
+                    api.call($commentBlock.data('url-new'), {
+                        dataType: 'json',
+                        success: function(ob){
+                            if(ob.form){
+                                var $newForm = $(ob.form);
+                                $commentBlockNew.html($newForm);
 
-                        $newForm.on('submit', function(e){
-                            e.preventDefault();
-                            e.stopPropagation();
+                                $newForm.on('submit', function(e){
+                                    e.preventDefault();
+                                    e.stopPropagation();
 
-                            window.lineStorm.api.saveForm($newForm, function(ob, status, xhr){
-                                if(xhr.status === 200){
-                                } else if(xhr.status === 201) {
-                                    $commentBlockComments.append(ob.html);
-                                    $commentBlockNew.empty();
-                                    loadCommentForm();
-                                } else {
-                                }
-                            });
+                                    api.saveForm($newForm, function(ob, status, xhr){
+                                        if(xhr.status === 200){
+                                        } else if(xhr.status === 201) {
+                                            $commentBlockComments.append(ob.html);
+                                            $commentBlockNew.empty();
+                                            _loadCommentForm();
+                                        } else {
+                                        }
+                                    });
 
-                            return false;
-                        });
-                    }
-                }
-            });
-        };
-
-
-        var loadComments = function(){
-            // load current comments
-            window.lineStorm.api.call($commentBlock.data('url-get'), {
-                dataType: 'html',
-                success: function(html){
-                    $commentBlockComments.html(html);
-                    $commentBlockNew.find('textarea,input').val('');
-                }
-            });
-            return false;
-        };
-
-        // init load comments
-        loadCommentForm();
-        loadComments();
-
-        // bind comment refresh
-        $('.comments-refresh').on('click', loadComments);
-        $('.comments-refresh-form').on('click', loadCommentForm);
-
-        $commentBlock.on('click', '.comment-reply', function(){
-            var $this = $(this);
-            var $comment = $this.closest('.comment-row');
-            window.lineStorm.api.call($this.data('url'), {
-                dataType: 'json',
-                success: function(ob){
-                    if(ob.form){
-                        var $newForm = $(ob.form);
-                        $comment.after($newForm);
-
-                        $newForm.on('submit', function(e){
-                            e.preventDefault();
-                            e.stopPropagation();
-
-                            window.lineStorm.api.saveForm($newForm, function(ob, status, xhr){
-                                if(xhr.status === 200){
-                                } else if(xhr.status === 201) {
-                                    $comment.find('.media-body').append(ob.html);
-                                    $newForm.remove();
-                                    $commentBlockNew.empty();
-                                    loadCommentForm();
-                                } else {
-                                }
-                            });
-
-                            return false;
-                        });
-                    }
-                }
-            });
-        });
-
-        // comment delete button
-        $commentBlock.on('click', '.comment-delete', function(){
-            if(confirm("Are you sure you want to delete this comment?")){
-                var $this = $(this);
-                window.lineStorm.api.call($this.data('url'), {
-                    method: 'DELETE',
-                    dataType: 'html',
-                    success: function(html){
-                        $this.closest('.comment-row').slideUp(1000, function(){
-                            $(this).remove();
-                        });
-                    }
-                });
-
-            }
-            return false;
-        });
-
-
-        $('.comment-block form').on('submit', function(e){
-            e.preventDefault();
-            e.stopPropagation();
-            $('#FormErrors').slideUp(function(){ $(this).html(''); });
-            window.lineStorm.api.saveForm($(this), function(on, status, xhr){
-                if(xhr.status === 200){
-                } else if(xhr.status === 201) {
-                    window.location = on.location;
-                } else {
-                }
-            }, function(e, status){
-                if(e.status === 400){
-                    if(e.responseJSON){
-                        var errors = window.lineStorm.api.parseError(e.responseJSON.errors);
-                        var str = '';
-                        for(var i in errors){
-                            if(errors[i].length)
-                                str += "<p class=''><strong style='text-transform:capitalize;'>"+i+":</strong> "+errors[i].join(', ')+"</p>";
+                                    return false;
+                                });
+                            }
                         }
-                        $('#FormErrors').html(str).slideDown();
-                    } else {
-                        alert(status);
+                    });
+                },
+
+                _loadComments = function(){
+                    // load current comments
+                    api.call($commentBlock.data('url-get'), {
+                        dataType: 'html',
+                        success: function(html){
+                            $commentBlockComments.html(html);
+                        }
+                    });
+                    return false;
+                }
+            ;
+
+            _loadCommentForm();
+            _loadComments();
+
+            // bind comment refresh
+            $('.comments-refresh').on('click', _loadComments);
+            $('.comments-refresh-form').on('click', _loadCommentForm);
+
+
+
+            $commentBlock.on('click', '.comment-reply', function(){
+                var $this = $(this);
+                var $comment = $this.closest('.comment-row');
+                api.call($this.data('url'), {
+                    dataType: 'json',
+                    success: function(ob){
+                        if(ob.form){
+                            var $newForm = $(ob.form);
+                            $comment.after($newForm);
+
+                            $newForm.on('submit', function(e){
+                                e.preventDefault();
+                                e.stopPropagation();
+
+                                api.saveForm($newForm, function(ob, status, xhr){
+                                    if(xhr.status === 200){
+                                    } else if(xhr.status === 201) {
+                                        $comment.find('.media-body').append(ob.html);
+                                        $newForm.remove();
+                                        $commentBlockNew.empty();
+                                        _loadCommentForm();
+                                    } else {
+                                    }
+                                });
+
+                                return false;
+                            });
+                        }
                     }
+                });
+            });
+
+            // comment delete button
+            $commentBlock.on('click', '.comment-delete', function(){
+                if(confirm("Are you sure you want to delete this comment?")){
+                    var $this = $(this);
+                    api.call($this.data('url'), {
+                        method: 'DELETE',
+                        dataType: 'html',
+                        success: function(html){
+                            $this.closest('.comment-row').slideUp(1000, function(){
+                                $(this).remove();
+                            });
+                        }
+                    });
+
+                }
+                return false;
+            });
+
+
+            $commentBlock.find('form').on('submit', function(e){
+                e.preventDefault();
+                e.stopPropagation();
+                $('#FormErrors').slideUp(function(){ $(this).html(''); });
+                api.saveForm($(this), function(on, status, xhr){
+                    if(xhr.status === 200){
+                    } else if(xhr.status === 201) {
+                        window.location = on.location;
+                    } else {
+                    }
+                }, function(e, status){
+                    if(e.status === 400){
+                        if(e.responseJSON){
+                            var errors = api.parseError(e.responseJSON.errors);
+                            var str = '';
+                            for(var i in errors){
+                                if(errors[i].length)
+                                    str += "<p class=''><strong style='text-transform:capitalize;'>"+i+":</strong> "+errors[i].join(', ')+"</p>";
+                            }
+                            $('#FormErrors').html(str).slideDown();
+                        } else {
+                            alert(status);
+                        }
+                    }
+                });
+
+                return false;
+            });
+
+            $commentBlock.find('.page-form-delete').on('click', function(){
+                if(confirm("Are you sure you want to permanently delete this page?")){
+                    api.call($(this).data('url'), {
+                        method: 'DELETE',
+                        success: function(o){
+                            alert(o.message);
+                            window.location = o.location;
+                        }
+                    });
                 }
             });
 
-            return false;
-        });
 
-        $('.page-form-delete').on('click', function(){
-            if(confirm("Are you sure you want to permanently delete this page?")){
-                window.lineStorm.api.call($(this).data('url'), {
-                    method: 'DELETE',
-                    success: function(o){
-                        alert(o.message);
-                        window.location = o.location;
-                    }
-                });
-            }
-        });
+            return {
+                comments: _loadComments,
+                form: _loadCommentForm
+            };
+        }
+    ;
 
-    });
-})();
+
+
+    return {
+        load: _load
+    };
+});
